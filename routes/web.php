2ebc87fc7a88ec0c6dfa70740c332aa10b235f69
mina hajian <mn.hajian@gmail.com>
2024-09-17 11:39:36 +0430

    init admin shop project and category in market and indx category

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\Market\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.home');
+    Route::prefix('market')->namespace('Markrt')->group(function () {
+        //category
+        Route::prefix('category')->group(function () {
+            Route::get('/', [CategoryController::class, 'index'])->name('admin.market.category.index');
+            Route::get('/create', [CategoryController::class, 'create'])->name('admin.market.category.create');
+            Route::post('/store', [CategoryController::class, 'store'])->name('admin.market.category.store');
+            Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('admin.market.category.edit');
+            Route::put('/update', [CategoryController::class, 'update'])->name('admin.market.category.update');
+            Route::delete('/delete/{id}', [CategoryController::class, 'destroy'])->name('admin.market.category.destroy');
+
+            //Route::resource('category', CategoryController::class);
+            // Route::resource('category', '\App\Http\Controllers\Admin\market\CategoryController');
+        });
+    });
+});
